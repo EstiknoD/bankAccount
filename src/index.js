478324cf520578e3091665aafcd97c742d0ed217
David <76698959+EstiknoD@users.noreply.github.com>
2021-01-12 10:14:37 +0100

    Starting the server

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
+const flash = require('connect-flash');
+const session = require('express-session');
+const mysqlstore = require('express-mysql-session');
+const passport = require('passport');
+
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+
+app.use(flash());
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port ', app.get('port'));
+});
