13485f15745c05fed6224f30484899e749ec00cd
David <76698959+EstiknoD@users.noreply.github.com>
2021-01-13 16:32:51 +0100

    Iniciando handlebars

--- src/routes/bank.js
+++ src/routes/bank.js
@@ -2,5 +2,16 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
+router.get('/', (req, res) => {
+    res.render('bank/account');
+});
+
+router.get('/main', (req, res) => {
+    res.render('bank/mainaccount');
+});
+
+router.get('/save', (req, res) => {
+    res.render('bank/saveaccount');
+});
 
 module.exports = router;
